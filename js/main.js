e922333f65ad0ad9c62e32c89ba38ec888e2a538
chandoul <94743119+chandoul@users.noreply.github.com>
2025-11-14 18:51:17 +0100

    Add download parameter to installer URL

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 let setting =
     "https://raw.githubusercontent.com/chandoul/aoeii_easy_manager/main/AoE%20II%20Manager.json";
 let installer =
-    "https://github.com/chandoul/aoeii_easy_manager/raw/refs/heads/main/bin/base/AoE%20II%20Manager%20AIO.exe";
+    "https://github.com/chandoul/aoeii_easy_manager/raw/refs/heads/main/bin/base/AoE%20II%20Manager%20AIO.exe?download=";
 let updater =
     "https://github.com/chandoul/aoeii_easy_manager/raw/refs/heads/main/bin/update/Age%20of%20Empires%20II%20Easy%20Manager%20%5B%20Update%20-%203.9%20%5D.exe";
 let ahkinstaller = "https://www.autohotkey.com/download/ahk-v2.exe";
